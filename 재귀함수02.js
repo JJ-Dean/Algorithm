49903a107e791360ffed0c148af0b0fb3179611b
Dean <yjj052800@gmail.com>
2022-12-22 20:15:48 +0900

    [재귀함수] 이진법 변환

---
+++ 재귀함수02.js
@@ -0,0 +1,34 @@
+// 2진수 변환
+
+// 반복문
+let result = '';
+let x = 11;
+while (true) {
+  if (x % 2 == 0) {
+    result += '0';
+  } else {
+    result += '1';
+  }
+  x = Math.floor(x / 2);
+  //   Math.ceil() : 소수점 올림
+  //   Math.floor() : 소수점 버림
+  //   Math.round() : 소수점 반올림
+  if (x == 1 || x == 0) {
+    result += String(x);
+    break;
+  }
+}
+console.log(result); //1101
+console.log(result.split('')); // [ '1', '1', '0', '1' ]
+console.log(result.split('').reverse()); // [ '1', '0', '1', '1' ]
+console.log(result.split('').reverse().join('')); // 1011
+
+// 재귀함수
+function 이진법(숫자) {
+  if (숫자 == 1 || 숫자 == 0) {
+    return String(숫자);
+  }
+  return 이진법(Math.floor(숫자 / 2)) + String(숫자 % 2);
+}
+
+console.log(이진법(11));
