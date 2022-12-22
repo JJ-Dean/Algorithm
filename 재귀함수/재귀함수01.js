c29e9154d965291b82dae5f28f0d8479de5ed65a
Dean <yjj052800@gmail.com>
2022-12-22 19:26:37 +0900

    반복문 -> 재귀함수로 표현

--- 재귀함수/재귀함수01.js
+++ 재귀함수/재귀함수01.js
@@ -16,6 +16,15 @@ console.log(s); // 5050
 console.log((n * (n + 1)) / 2); // 5050
 // O(1)
 
+// 재귀함수를 이용한 1부터 100까지의 합
+function f(n) {
+  if (n <= 1) {
+    return 1;
+  }
+  return n + f(n - 1);
+}
+console.log(`재귀함수 : ${f(100)}`); // 재귀함수 : 5050
+
 // 반복문을 이용한 1부터 6까지의 곱
 let m = 1;
 
@@ -23,3 +32,12 @@ for (let i = 1; i < 6; i++) {
   m *= i;
 }
 console.log(m); // 120
+
+// 재귀함수를 이용한 1부터 100까지의 합
+function q(l) {
+  if (l <= 1) {
+    return 1;
+  }
+  return l * q(l - 1);
+}
+console.log(`재귀함수 : ${q(5)}`); // 재귀함수 : 120
